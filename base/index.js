51068b31aa908a7d806fc5a151cada0556fb632b
sbarton237 <72349665+sbarton237@users.noreply.github.com>
2024-01-19 15:05:56 -0500

    step 2

---
+++ base/index.js
@@ -0,0 +1,16 @@
+//https://www.freecodecamp.org/news/how-to-build-a-multiplayer-card-game-with-phaser-3-express-and-socket-io/
+//tutorial on making a multiplayer card game, am adpating to suit my own purposes
+import Phaser from "phaser";
+import game from "./scenes/game";
+
+const config = {
+    type: Phaser.AUTO,
+    parent: "phaser-example",
+    width: 1280,
+    height: 780,
+    scene: [
+        Game
+    ]
+};
+
+const game = new Phaser.Game(config);
--- scenes/game.js
+++ scenes/game.js
@@ -64,9 +64,35 @@ export default class Game extends Phaser.Scene {
 
     create() {
         this.dealText = this.add.text(75, 350, ['Deal Cards']).setFontSize(18).setFontFamily('Lucida Sans').setColor('#000000').setInteractive();
+
+        let self = this;
+
+        this.card = this.add.image(300, 300, 'back').setScale(0.3, 0.3).setInteractive();
+        this.input.setDraggable(this.card);
+
+        this.dealCards = () => {
+
+        }
+
+        this.dealText.on('pointerdown', function () {
+            self.dealCards();
+        })
+
+        this.dealText.on('pointerover', function () {
+            self.dealText.setColor('#777777')
+        })
+
+        this.dealText.on('pointerout', function () {
+            self.dealText(setColor('000000'))
+        })
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        })
     }
 
     update() {
-        
+
     }
 }
