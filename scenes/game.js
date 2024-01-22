c6efce55e94ac06f8c0c8afbdbcfb92d092eeaad
sbarton237 <72349665+sbarton237@users.noreply.github.com>
2024-01-22 12:50:39 -0500

    step 4

--- scenes/game.js
+++ scenes/game.js
@@ -1,4 +1,5 @@
 import Card from '../helpers/card';
+import Zone from '../helpers/zone';
 
 export default class Game extends Phaser.Scene {
     constructor() {
@@ -72,6 +73,10 @@ export default class Game extends Phaser.Scene {
         this.card = this.add.image(300, 300, 'back').setScale(0.3, 0.3).setInteractive();
         this.input.setDraggable(this.card);
 
+        this.zone = new Zone(this);
+        this.dropZone = this.zone.renderZone();
+        this.outline = this.zone.renderOutline(this.dropZone);
+
         this.dealCards = () => {
             for ( let i = 0; i < 4; i ++) {
                 let playerCard = new Card(this);
@@ -94,6 +99,26 @@ export default class Game extends Phaser.Scene {
         this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
             gameObject.x = dragX;
             gameObject.y = dragY;
+        })
+
+        this.input.on('dragstart', function (pointer, gameObject) {
+            gameObject.setTint(0xff69b4);
+            self.children.bringToTop(gameObject);
+        })
+
+        this.input.on('dragend', function (point, gameObject, dropped) {
+            gameObject.setTint();
+            if (!dropped) {
+                gameObject.x = gameObject.input.dragStartX;
+                gameObject.y = gameObject.input.dragStartY;
+            }
+        })
+
+        this.input.on('drop', function (pointer, gameObject, dropZone) {
+            dropZone.data.values.cards++;
+            gameObject.x = (dropZone.x - 350) + (dropZone.data.values.cards * 50);
+            gameObject.y = dropZone.y;
+            gameObject.disableInteractive();
         })
     }
 
