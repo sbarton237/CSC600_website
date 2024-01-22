e114310a248ba79153de194a8feaf3fde99cac0b
sbarton237 <72349665+sbarton237@users.noreply.github.com>
2024-01-22 14:00:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //https://www.freecodecamp.org/news/how-to-build-a-multiplayer-card-game-with-phaser-3-express-and-socket-io/
 //tutorial on making a multiplayer card game, am adpating to suit my own purposes
 import Phaser from "phaser";
-import game from "./scenes/game";
+import Game from "./scenes/game";
 
 const config = {
     type: Phaser.AUTO,
